d12028c2753944094bb17c3c279b029de3da469f
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2012-06-27 20:11:36 +0200

    covariance now puts data in database instead of printing them out. - NOT TESTED

--- computeCovariance.py
+++ computeCovariance.py
@@ -18,14 +18,22 @@ def usage():
 """
 
 def getVectors(cursor):
+	global _debug
+	if _debug == 1:
+		print "reading out data..."
 	statement = "SELECT d_1"
 	for i in range(2,129):
 		statement += ", d_%d" %i
 	statement += " from descriptors"
 	cursor.execute(statement)
-	return cursor.fetchall()
+	ret = cursor.fetchall()
+	if _debug == 1:
+		print "done."
+	return ret
 	
 def setUpDatabase(database):
+	if _debug == 1:
+		print "setting up database..."
 	c = conn.cursor()
 	c.execute("DROP Table if exists eigendata");
         statement = "CREATE TABLE eigendata (eigenvalue float "
@@ -36,8 +44,19 @@ def setUpDatabase(database):
 	conn.commit()
 	c.close()
 
+def save(eval,evec):
+	for i in range(1,len(eval)):
+		statement = "INSERT INTO eigendata VALUES (\"%f\"" % eval[i]
+        	for ev in evec[i]:
+        		statement += ",%f" % ev
+	        #statement = statement[:len(statement)-2]
+        	statement += ")"
+	        cursor.execute(statement)
+
 
 if __name__=="__main__":
+	global _debug 
+	_debug = 0
 	try:
 		database = "surf_descriptors.db"
 		opts, args = getopt.getopt(sys.argv[1:], "hvb:", ["help", "verbose","database"])
@@ -46,7 +65,6 @@ if __name__=="__main__":
 				usage()                     
 				sys.exit()                  
 			elif opt in ("-v", "--verbose"):
-				global _debug               
 				_debug = 1                  
 			elif opt in ("-b", "--database"):
 				database = arg
@@ -57,9 +75,16 @@ if __name__=="__main__":
 		setUpDatabase(database)
 		c = conn.cursor()
 		#covariance-matrix, 128x128
+		if _debug == 1:
+			print "calculating covariance after reading out data..."
 		cov = numpy.corrcoef(getVectors(c), rowvar=0)
 		#get eigenvalues and eigenvectors
+		if _debug == 1:
+			print "calculating eigenvalues/wigenvectors..."
 		eval, evec = numpy.linalg.eig(cov)
+		if _debug == 1:
+			print "saving..."
+		save(eval,evec)
 		print "eigenvalues: %s" % eval[:30]
 		print "eigenvectors: %s" % evec[:30]
 	except getopt.GetoptError:           
